948b365e65009a691800d41816ca8fe48065c6b1
kazamajinz <kazamajinz@naver.com>
2023-08-23 13:00:35 +0900

    파일이름 변경, 함수temp_B, 함수temp_C 추가

--- GitConfilictNaming/UIApplication+extension.swift
+++ GitConfilictNaming/UIApplication+extension.swift
@@ -1,5 +1,5 @@
 //
-//  UIApplication+width.swift
+//  UIApplication+extension.swift
 //  GitConfilictNaming
 //
 //  Created by 이정환 on 2023/08/23.
@@ -9,4 +9,6 @@ import UIKit
 
 extension UIApplication {
     func temp_A() { }
+    func temp_B() { }
+    func temp_C() { }
 }
